6a14c31fc9c3b682669c2ff37ef89f2c165b62b4
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-08-06 15:13:35 +0200

    validity filter bug (no more date parsing)

--- core/utils.py
+++ core/utils.py
@@ -27,9 +27,6 @@ def filter_validity(arg='validity', **kwargs):
     validity = kwargs.get(arg)
     if validity is None:
         validity = core.datetime.datetime.now()
-    else:
-        d = re.split('\D', validity)
-        validity = core.datetime.datetime(*[int('0'+x) for x in d][:6])
     return (
         Q(validity_from=None) | Q(validity_from__lte=validity),
         Q(validity_to=None) | Q(validity_to__gte=validity)
