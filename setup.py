4c6b4c8b8da8f32555928ce1590e239657f73ab7
Xavier Gillmann <xgillmann@bluesquarehub.com>
2020-03-27 14:34:30 +0100

    bumped v1.0.13

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='openimis-be-core',
-    version='1.0.12',
+    version='1.0.13',
     packages=find_packages(),
     include_package_data=True,
     license='GNU AGPL v3',
