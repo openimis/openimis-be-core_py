9ff4619f2b167d072bd9f036224d021b64d75b76
Damian Borowiecki <dborowiecki@soldevelo.com>
2021-01-13 09:52:58 +0100

    OAI-29: Added websocket client to backend

--- core/__init__.py
+++ core/__init__.py
@@ -2,5 +2,6 @@ from .utils import *
 from .datetimes.shared import *
 from django.db.models import Q
 from .custom_lookups import *
+import websocket
 
 default_app_config = 'core.apps.CoreConfig'
---
+++ core/websocket/__init__.py
@@ -0,0 +1,3 @@
+from .abstract_websocket_client import AbstractWebSocketClient
+from .base_websocket_client import BaseWebSocketClient, JsonWebSocketClient
+from .async_websocket_client import AsyncWebSocketClient
