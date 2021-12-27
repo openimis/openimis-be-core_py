a2166b49a38b3e2fc95b5685f64bab6d82550976
Quentin Gérôme <geromequentin@gmail.com>
2021-12-27 15:01:00 +0000

    feat(JWT): Implement JWT authentication

--- core/jwt.py
+++ core/jwt.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__file__)
 def jwt_encode_user_key(payload, context=None):
     token = jwt.encode(
         payload,
-        get_jwt_key(encode=True, context=context),
+        get_jwt_key(encode=True, context=context, payload=payload),
         jwt_settings.JWT_ALGORITHM,
     )
     # JWT module after 1.7 does the encoding, introducing some conflicts in graphql-jwt, let's support both
@@ -27,9 +27,9 @@ def jwt_decode_user_key(token, context=None):
         token,
         get_jwt_key(encode=False, context=context),
         options={
-            'verify_exp': jwt_settings.JWT_VERIFY_EXPIRATION,
-            'verify_aud': jwt_settings.JWT_AUDIENCE is not None,
-            'verify_signature': False,
+            "verify_exp": jwt_settings.JWT_VERIFY_EXPIRATION,
+            "verify_aud": jwt_settings.JWT_AUDIENCE is not None,
+            "verify_signature": False,
         },
         leeway=jwt_settings.JWT_LEEWAY,
         audience=jwt_settings.JWT_AUDIENCE,
@@ -38,10 +38,11 @@ def jwt_decode_user_key(token, context=None):
     )
     if not_validated and not_validated.get("username"):
         user_class = apps.get_model("core", "User")
-        db_user = user_class.objects\
-            .filter(username=not_validated.get("username"))\
-            .only("i_user__private_key")\
+        db_user = (
+            user_class.objects.filter(username=not_validated.get("username"))
+            .only("i_user__private_key")
             .first()
+        )
         if db_user and db_user.private_key:
             key = db_user.private_key
         else:
@@ -52,9 +53,9 @@ def jwt_decode_user_key(token, context=None):
         token,
         key,
         options={
-            'verify_exp': jwt_settings.JWT_VERIFY_EXPIRATION,
-            'verify_aud': jwt_settings.JWT_AUDIENCE is not None,
-            'verify_signature': jwt_settings.JWT_VERIFY,
+            "verify_exp": jwt_settings.JWT_VERIFY_EXPIRATION,
+            "verify_aud": jwt_settings.JWT_AUDIENCE is not None,
+            "verify_signature": jwt_settings.JWT_VERIFY,
         },
         leeway=jwt_settings.JWT_LEEWAY,
         audience=jwt_settings.JWT_AUDIENCE,
@@ -63,10 +64,13 @@ def jwt_decode_user_key(token, context=None):
     )
 
 
-def get_jwt_key(encode=True, context=None):
+def get_jwt_key(encode=True, context=None, payload=None):
     user_key = extract_private_key_from_context(context)
+    if user_key is None and payload is not None:
+        user_key = extract_private_key_from_payload(payload)
     if user_key:
         return user_key
+
     if encode:
         if hasattr(jwt_settings, "JWT_PRIVATE_KEY"):
             return jwt_settings.JWT_PRIVATE_KEY
@@ -74,6 +78,14 @@ def get_jwt_key(encode=True, context=None):
         if hasattr(jwt_settings, "JWT_PUBLIC_KEY"):
             return jwt_settings.JWT_PUBLIC_KEY
     return jwt_settings.JWT_SECRET_KEY
+
+
+def extract_private_key_from_payload(payload):
+    # Get user private key from payload. This covers the refresh token mutation
+    from core.models import User
+
+    if "username" in payload:
+        return User.objects.get(username=payload["username"]).private_key
 
 
 def extract_private_key_from_context(context):
