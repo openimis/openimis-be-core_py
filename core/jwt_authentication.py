5d8de33d0887e4272a8b0cd66713bfde70e055cd
Quentin Gérôme <geromequentin@gmail.com>
2022-02-01 14:29:48 +0000

    Prettify jwt_authentication.py

--- core/jwt_authentication.py
+++ core/jwt_authentication.py
@@ -15,46 +15,51 @@ logger = logging.getLogger(__file__)
 
 class JWTAuthentication(BaseAuthentication):
     """
-        class to obtain token from header if it is provided
-        and verify if this is correct/valid token
+    class to obtain token from header if it is provided
+    and verify if this is correct/valid token
     """
 
     def authenticate(self, request):
-        authorization_header = request.headers.get('Authorization')
+        authorization_header = request.headers.get("Authorization")
         if not authorization_header:
             return None
         payload = None
-        is_remote_user_auth = os.getenv('REMOTE_USER_AUTHENTICATION', 'False')
-        if authorization_header.lower().startswith('bearer '):
-            bearer, access_token, *extra_words = authorization_header.split(' ')
+        is_remote_user_auth = os.getenv("REMOTE_USER_AUTHENTICATION", "False")
+        if authorization_header.lower().startswith("bearer "):
+            bearer, access_token, *extra_words = authorization_header.split(" ")
             if len(extra_words) > 0:
                 raise exceptions.AuthenticationFailed("Improper structure of token")
             try:
                 payload = jwt_decode_user_key(token=access_token)
             except jwt.DecodeError:
-                raise exceptions.AuthenticationFailed('Error on decoding token')
+                raise exceptions.AuthenticationFailed("Error on decoding token")
             except jwt.ExpiredSignatureError:
-                raise exceptions.AuthenticationFailed('Access_token expired')
+                raise exceptions.AuthenticationFailed("Access_token expired")
             except IndexError:
-                raise exceptions.AuthenticationFailed('Token prefix missing')
+                raise exceptions.AuthenticationFailed("Token prefix missing")
             user = None
             if payload:
                 user_class = apps.get_model("core", "User")
-                user = user_class.objects \
-                    .filter(username=payload.get("username")) \
-                    .only("i_user__private_key") \
+                user = (
+                    user_class.objects.filter(username=payload.get("username"))
+                    .only("i_user__private_key")
                     .first()
+                )
             if user is None:
-                raise exceptions.AuthenticationFailed('User inactive or deleted/not existed.')
+                raise exceptions.AuthenticationFailed(
+                    "User inactive or deleted/not existed."
+                )
             if not user.is_active:
-                raise exceptions.AuthenticationFailed('User is inactive')
-        elif is_remote_user_auth.lower() == 'true':
+                raise exceptions.AuthenticationFailed("User is inactive")
+        elif is_remote_user_auth.lower() == "true":
             # support for basic auth when we have in backend 'api' instead of 'iapi'
-            if authorization_header.lower().startswith('basic'):
+            if authorization_header.lower().startswith("basic"):
                 if self._check_basic_auth_user_exist(authorization_header):
                     return None
             else:
-                raise exceptions.AuthenticationFailed("Basic auth error: there is no basic header")
+                raise exceptions.AuthenticationFailed(
+                    "Basic auth error: there is no basic header"
+                )
         else:
             raise exceptions.AuthenticationFailed("Missing 'Bearer' prefix")
 
@@ -64,16 +69,3 @@ class JWTAuthentication(BaseAuthentication):
 
     def enforce_csrf(self, request):
         return  # To not perform the csrf during checking auth header
-
-    def _check_basic_auth_user_exist(self, auth_header):
-        """when we have 'api' root instead of 'iapi' - check user by using basic auth"""
-        token_type, _, credentials = auth_header.partition(' ')
-        # get username from base token
-        username = base64.b64decode(credentials).decode("utf8").split(':', 1)[0]
-        # check if user exists in db
-        user_class = apps.get_model("core", "User")
-        user = user_class.objects.filter(username=username).first()
-        if user:
-            return True
-        else:
-            raise exceptions.AuthenticationFailed("Basic auth error: such user doesn't exist")
