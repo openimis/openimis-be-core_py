05e5c1423500d369c5890144a57e73c216fd5a54
sniedzielski <sniedzielski@soldevelo.com>
2022-05-09 15:26:28 +0200

    OP-766: fix migration to check if lang exists or not

--- core/migrations/0017_add_gambian_english.py
+++ core/migrations/0017_add_gambian_english.py
@@ -6,9 +6,12 @@ logger = logging.getLogger(__name__)
 
 
 MIGRATION_SQL = """
-    INSERT INTO imis.dbo.tblLanguages
-    (LanguageCode, LanguageName, SortOrder)
-    VALUES('en-gm', 'Gambian English ', 3);
+    IF NOT EXISTS (SELECT * FROM imis.dbo.tblLanguages WHERE LanguageCode = 'en-gm')
+    BEGIN
+        INSERT INTO imis.dbo.tblLanguages
+        (LanguageCode, LanguageName, SortOrder)
+        VALUES('en-gm', 'Gambian English ', 3);
+    END
 """
 
 
