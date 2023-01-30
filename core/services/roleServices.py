d9ce93cfb529427bcf81f03f55dbe48d020f7880
jdolkowski <jdolkowski@soldevelo.com>
2023-01-30 16:16:21 +0100

    OP-1082: change Location to Role in validation error message

--- core/services/roleServices.py
+++ core/services/roleServices.py
@@ -4,5 +4,5 @@ from core.models import Role
 def check_role_unique_name(name, uuid=None):
     query = Role.objects.filter(name=name, validity_to__isnull=True)
     if query.exists() and (not uuid or query.get().uuid != uuid):
-        return [{"message": "Location code %s already exists" % name}]
+        return [{"message": "Role code %s already exists" % name}]
     return []
