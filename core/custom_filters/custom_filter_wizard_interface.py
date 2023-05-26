71ec18f50c1e6be99aafc5b31a4d2c5cd6db3dec
sniedzielski <sniedzielski@soldevelo.com>
2023-05-26 11:24:43 +0200

    CM-68: fixed docstring for interface method of loading definition

--- core/custom_filters/custom_filter_wizard_interface.py
+++ core/custom_filters/custom_filter_wizard_interface.py
@@ -13,9 +13,11 @@ class CustomFilterWizardInterface:
         pass
 
     def load_definition(self, tuple_type: type) -> List[namedtuple]:
-        """Load the definition how to create filters"""
         """
-            Load the definition how to create filters
+            Load the definition how to create filters. The output is simply the list
+            of named tuple (from collections package). Such named tuple is built in such way:
+            <Type>(field=<str>, filter=<str>, value=<str>) for example:
+            BenefitPlan(field='income', filter='lt, gte, icontains, exact', value='')
 
             :param tuple_type: the type of tuple
              
