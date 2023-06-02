b656d9115da74744f46033e276a6c87765eea9d5
sniedzielski <sniedzielski@soldevelo.com>
2023-06-02 11:44:30 +0200

    CM-68: added changes after review

--- core/custom_filters/custom_filter_wizard_interface.py
+++ core/custom_filters/custom_filter_wizard_interface.py
@@ -3,29 +3,30 @@ from typing import List
 
 
 class CustomFilterWizardInterface:
+
     def get_type_of_object(self) -> str:
         """
-            Get the type of object for which we want to define a specific way of building filters.
+        Get the type of object for which we want to define a specific way of building filters.
 
-            Returns:
-                str: The type of the object.
+        :return: The type of the object.
+        :rtype: str
         """
         pass
 
     def load_definition(self, tuple_type: type, **kwargs) -> List[namedtuple]:
         """
-            Load the definition of how to create filters.
+        Load the definition of how to create filters.
 
-            This method retrieves the definition of how to create filters and returns it as a list of named tuples.
-            Each named tuple is built with the provided `tuple_type` and has the fields `field`, `filter`, and `value`.
+        This method retrieves the definition of how to create filters and returns it as a list of named tuples.
+        Each named tuple is built with the provided `tuple_type` and has the fields `field`, `filter`, and `value`.
 
-            Example named tuple: <Type>(field=<str>, filter=<str>, value=<str>)
-            Example usage: BenefitPlan(field='income', filter='lt, gte, icontains, exact', value='')
+        Example named tuple: <Type>(field=<str>, filter=<str>, value=<str>)
+        Example usage: BenefitPlan(field='income', filter='lt, gte, icontains, exact', value='')
 
-            Args:
-                tuple_type (type): The type of the named tuple.
+        :param tuple_type: The type of the named tuple.
+        :type tuple_type: type
 
-            Returns:
-                List[namedtuple]: A list of named tuples representing the definition of how to create filters.
+        :return: A list of named tuples representing the definition of how to create filters.
+        :rtype: List[namedtuple]
         """
         pass
