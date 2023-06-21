0451f93e2bbb579cd8e4987d26a726d1d76be89d
sniedzielski <52816247+sniedzielski@users.noreply.github.com>
2023-06-21 11:43:06 +0200

    CM-162: improve custom filter applying into queryset in query and exports (#191)
    * CM-162: improve custom filter applying into queryset in query and exports
    
    * CM-162: fixed broken tests
    
    * CM-162: revert change with fix broken test
    
    * CM-162: changed username value to more unique

--- core/custom_filters/custom_filter_wizard_storage.py
+++ core/custom_filters/custom_filter_wizard_storage.py
@@ -1,6 +1,7 @@
 import logging
 
 from collections import namedtuple
+from django.db.models.query import QuerySet
 from typing import List
 
 from core.custom_filters.custom_filter_registry_point import CustomFilterRegistryPoint
@@ -62,6 +63,46 @@ class CustomFilterWizardStorage:
                     cls.__run_load_definition_object_in_wizard(wizard_filter_class, **kwargs)
                 )
         return output_of_possible_filters
+
+    @classmethod
+    def build_custom_filters_queryset(
+        cls,
+        module_name: str,
+        object_type: str,
+        custom_filters: List[namedtuple],
+        query: QuerySet,
+        relation=None,
+        **kwargs
+    ) -> QuerySet:
+        """
+        Apply custom filters to a queryset based on specific object provided by the definition from particular module.
+
+        :param module_name: The name of the module installed in openIMIS, required to retrieve information
+        about the possible ways of building filters for that specific module.
+
+        :param object_type: The name of the object type for which the filter building information is neede
+
+        :param custom_filters: Structure of custom filter tuple: <Type>(field=<str>, filter=<str>, type=<str>).
+        Example usage of filter tuple: BenefitPlan(field='income', filter='lt, gte, icontains, exact', type='integer')
+
+        :param query: The original queryset with filters for example: Queryset[Beneficiary].
+
+        :param relation: The optional argument which defines the relation field in queryset for example 'beneficiary'
+        Used as the prefix in queryset for example: beneficiary__json_ext__income__gte=1000
+        :type relation: str or None
+
+        :return: The updated queryset with additional filters applied for example: Queryset[Beneficiary].
+        """
+        registered_filter_wizards = CustomFilterRegistryPoint.REGISTERED_CUSTOM_FILTER_WIZARDS
+        if module_name in registered_filter_wizards:
+            for registered_filter_wizard in registered_filter_wizards[module_name]:
+                if cls.__KEY_FOR_OBTAINING_CLASS not in registered_filter_wizard:
+                    continue
+                wizard_filter_class = cls.__create_instance_of_wizard_class(registered_filter_wizard)
+                if not cls.__check_object_type(wizard_filter_class, object_type):
+                    continue
+                query = wizard_filter_class.apply_filter_to_queryset(custom_filters, query, relation)
+        return query
 
     @classmethod
     def __run_load_definition_object_in_wizard(
