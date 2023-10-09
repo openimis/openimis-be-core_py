2c36378f8a73c59cd2bca2d803dfbaaf3b70b592
wzglinieckisoldevelo <98958634+wzglinieckisoldevelo@users.noreply.github.com>
2023-10-09 09:40:51 +0200

    ONI-174: Add util to remove permission from a role. (#223)

--- core/utils.py
+++ core/utils.py
@@ -266,3 +266,17 @@ def insert_role_right_for_system(system_role, right_id):
         role_right = RoleRight.objects.create(role=existing_role, right_id=right_id)
 
     return role_right
+
+
+def remove_role_right_for_system(system_role, right_id):
+    RoleRight = apps.get_model("core", "RoleRight")
+    Role = apps.get_model("core", "Role")
+    existing_role = Role.objects.filter(is_system=system_role, validity_to__isnull=True).first()
+    if not existing_role:
+        logger.warning("Migration requested to remove a role_right for system role %s but couldn't find that role", system_role)
+    role_right = RoleRight.objects.filter(role=existing_role, right_id=right_id).first()
+    if role_right:
+        role_right.delete()
+        logger.info("Role right removed for system role %s and right ID %s", system_role, right_id)
+    else:
+        logger.warning("Role right not found for system role %s and right ID %s", system_role, right_id)
