316aa3dd60678941fbf2ae634394fff171de45bb
sniedzielski <52816247+sniedzielski@users.noreply.github.com>
2023-10-19 14:42:45 +0200

    CQI-80: OWASP A01:2021-Broken Access Control - openimis-be-core_py BE (#226)
    * CQI-80: removed setting locked password if nothing is passed in password
    
    * CQI-80: added require GET decorator into fetch_export view
    
    * CQI-80: added require GET decorator into get_scheduled_jobs
    
    * CQI-80: added require GET decorator into get_scheduled_jobs
    
    * CQI-80: revert change with password issue

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 import csv
 
 from django.http import Http404, StreamingHttpResponse
+from django.views.decorators.http import require_GET
 from isodate import strftime
 from rest_framework import viewsets, status
 from rest_framework.decorators import action, api_view
@@ -27,6 +28,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
 
 @api_view(['GET'])
+@require_GET
 def fetch_export(request):
     requested_export = request.query_params.get('export')
     export = ExportableQueryModel.objects.filter(name=requested_export).first()
@@ -51,5 +53,6 @@ def _serialize_job(job):
 
 
 @api_view(['GET'])
+@require_GET
 def get_scheduled_jobs(request):
     return Response([_serialize_job(job) for job in scheduler.get_jobs()])
