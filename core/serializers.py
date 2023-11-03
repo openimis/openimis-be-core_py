762a1c6108b4b71009d2df5bd98ccee9bef03d00
Jan <j.dolkowski@soldevelo.com>
2023-11-03 17:57:52 +0100

    OM-70: serialize user roles

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, InteractiveUser, TechnicalUser
+from .models import User, InteractiveUser, TechnicalUser, UserRole, Role
 
 
 class InteractiveUserSerializer(serializers.ModelSerializer):
@@ -17,10 +17,19 @@ class TechnicalUserSerializer(serializers.ModelSerializer):
         fields = ('id', 'language', 'username', 'email')
 
 
+from rest_framework import serializers
+
+
 class UserSerializer(serializers.ModelSerializer):
     i_user = InteractiveUserSerializer(many=False, read_only=True)
     t_user = TechnicalUserSerializer(many=False, read_only=True)
+    user_roles = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'i_user', 't_user')
+        fields = ('id', 'username', 'i_user', 't_user', 'user_roles')
+
+    def get_user_roles(self, obj):
+        user_roles = UserRole.objects.filter(user=obj.i_user)
+        role_names = [user_role.role.name for user_role in user_roles]
+        return role_names
