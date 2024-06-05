cba04a4a0d68bbd08bb774e3277ff464d3f2a8d7
hirensoni913 <hirensoni913@gmail.com>
2024-06-05 10:45:54 +0200

    Added a class to remove any indexes for a column

---
+++ core/services/utils/remove_indexes.py
@@ -0,0 +1,22 @@
+from django.db import connection, migrations
+
+
+class RemoveIndexForField(migrations.RunPython):
+    def __init__(self, app_name, model_name, field_name) -> None:
+        self.app_name = app_name
+        self.model_name = model_name
+        self.field_name = field_name
+        super().__init__(self.remove_index, self.reverse_remove_index)
+
+    def remove_index(self, app, schema_editor):
+        model = app.get_model(self.app_name, self.model_name)
+        field = model._meta.get_field(self.field_name)
+
+        with connection.cursor() as cursor:
+            constraints = connection.introspection.get_constraints(cursor, model._meta.db_table)
+            for constraint_name, constraint_info in constraints.items():
+                if constraint_info["index"] and any(field.column.lower() == col.lower() for col in constraint_info["columns"]):
+                    cursor.execute(f"DROP INDEX {constraint_name} ON {model._meta.db_table}")
+
+    def reverse_remove_index(self, app, schema_editor):
+        pass
