2d90f07cf0f7a731e5e3cc947f9ce8995dcded53
delcroip <patrick.delcroix@swisstph.ch>
2024-09-19 13:48:38 +0200

    fi UUID

--- core/schema.py
+++ core/schema.py
@@ -1555,7 +1555,7 @@ def update_or_create_user(data, user):
         data.pop('client_mutation_label')
     user_uuid = data.pop('uuid') if 'uuid' in data else None
 
-    if uuid.UUID(user_uuid) == uuid.UUID(user.id) and user.is_imis_admin and imis_administrator_system not in data.get("roles", []):
+    if uuid.UUID(str(user_uuid)) == uuid.UUID(str(user.id)) and user.is_imis_admin and imis_administrator_system not in data.get("roles", []):
         raise ValidationError("Administrator cannot deprovision himself.")
 
     if UT_INTERACTIVE in data["user_types"]:
