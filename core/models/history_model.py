645a12cc458aec4eb5bac21550d23a9f09e1adcd
delcroip <patrick.delcroix@swisstph.ch>
2026-02-20 10:03:54 +0100

    refactor(core): reorganize business models by adding UserBusinessAccess and ValidityMixin
    - Added UserBusinessAccess model with admin registration for user-specific business access control
    - Imported and exported ValidityMixin, OpenIMISBusinessModel, and UserBusinessAccess in models init
    - Removed HistoryBusinessModel class from history_model.py, likely moved to openimis_model for better separation of concerns
    - This refactoring improves code organization by centralizing business-related models and mixins, enhancing maintainability and modularity in the OpenIMIS core module.

--- core/models/history_model.py
+++ core/models/history_model.py
@@ -14,6 +14,7 @@ from django.db.models import (
 )
 from simple_history.models import HistoricalRecords
 from django.apps import apps
+from core.models.openimis_model import ValidityMixin
 
 logger = logging.getLogger(__name__)
 
@@ -317,69 +318,6 @@ class HistoryModel(DirtyFieldsMixin, CachedModelMixin, Model):
         abstract = True
 
 
-class HistoryBusinessModel(HistoryModel):
-    date_valid_from = DateTimeField(db_column="DateValidFrom", default=py_datetime.now)
-    date_valid_to = DateTimeField(db_column="DateValidTo", blank=True, null=True)
-    replacement_uuid = models.UUIDField(
-        db_column="ReplacementUUID", blank=True, null=True
-    )
-
-    def replace_object(self, data, **kwargs):
-        from .user import User
-        # check if object was created and saved in database (having date_created field)
-        if self.id is None:
-            return None
-        user = User.objects.get(**kwargs)
-        # 1 step - create new entity
-        new_entity = self._create_new_entity(user=user, data=data)
-        # 2 step - update the fields for the entity to be replaced
-        self._update_replaced_entity(
-            user=user,
-            uuid_from_new_entity=new_entity.id,
-            date_valid_from_new_entity=new_entity.date_valid_from,
-        )
-
-    def _create_new_entity(self, user, data):
-        """1 step - create new entity"""
-        now = py_datetime.now()
-        new_entity = copy(self)
-        new_entity.id = None
-        new_entity.version = 1
-        new_entity.date_valid_from = now
-        new_entity.date_valid_to = None
-        new_entity.replacement_uuid = None
-        # replace the fiedls if there are any to update in new entity
-        if "uuid" in data:
-            data.pop("uuid")
-        if len(data) > 0:
-            [setattr(new_entity, key, data[key]) for key in data]
-        if self.date_valid_from is None:
-            raise ValidationError("Field date_valid_from should not be empty")
-        new_entity.save(user=user)
-        return new_entity
-
-    def _update_replaced_entity(
-        self, user, uuid_from_new_entity, date_valid_from_new_entity
-    ):
-        """2 step - update the fields for the entity to be replaced"""
-        # convert to datetime if the date_valid_from from new entity is date
-        if not isinstance(date_valid_from_new_entity, base_datetime.datetime):
-            date_valid_from_new_entity = base_datetime.combine(
-                date_valid_from_new_entity, base_datetime.min.time()
-            )
-        if not self.is_dirty(check_relationship=True):
-            if self.date_valid_to is not None:
-                if date_valid_from_new_entity < self.date_valid_to:
-                    self.date_valid_to = date_valid_from_new_entity
-            else:
-                self.date_valid_to = date_valid_from_new_entity
-            self.replacement_uuid = uuid_from_new_entity
-            self.save(user=user)
-            return self
-        else:
-            raise ValidationError(
-                "Object is changed - it must be updated before being replaced"
-            )
-
+class HistoryBusinessModel(HistoryModel, ValidityMixin):
     class Meta:
         abstract = True
